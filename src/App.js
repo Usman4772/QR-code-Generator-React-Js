a108fe2e178ebc86f26e651deb3d0f9edf0f9a97
usman ali <usmanali477275@gmail.com>
2024-03-16 16:57:40 +0500

    Qr code Generator Using React Js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import QRCode from 'react-qr-code';
+import "./qrcode.css"
 
 function App() {
+  const [input,setInput]=useState("")
+  const [data,setData]=useState("")
+function handleGenerate(){
+  setData(input)
+  setInput("")
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+    <h1>QR Code Generator</h1>
+    <input type='text' value={input} onChange={(e)=>setInput(e.target.value)} placeholder='Enter your Text...'/>
+    <button disabled={input ===""?true:false} onClick={handleGenerate}>Generate</button>
+<QRCode 
+id='qr-code'
+value={data}
+size={100}
+></QRCode>
+<div className='prev-text'> Your Text Was : {data}</div>
     </div>
   );
 }
